ed1c9b46e339b1afc67c8dbf0f847d856d19d91d
Manuel Duarte <carlosd99m@gmail.com>
2022-02-26 21:25:31 -0600

    Initial Commit

---
+++ Shared/CarouselListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselListApp.swift
+//  Shared
+//
+//  Created by Manuel Duarte on 26/2/22.
+//
+
+import SwiftUI
+
+@main
+struct CarouselListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Manuel Duarte on 26/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
